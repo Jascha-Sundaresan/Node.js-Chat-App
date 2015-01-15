a159f5b10117e04b39b9b942324a7ff84ebfd6d6
fliz <flizarthanon@gmail.com>
2015-01-15 13:39:53 -0800

    build nickname functionality

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -11,4 +11,13 @@
     this.socket.emit('message', { text: message });
   }
 
+  Chat.prototype.processCommand = function(command) {
+    var parsedCommand = command.split(' ');
+    if (parsedCommand[0] === "nick") {
+      this.socket.emit('nicknameChangeRequest', parsedCommand[1]);
+    } else {
+      this.socket.emit('message', { text: 'not a command' });
+    }
+  };
+
 })();
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -5,7 +5,34 @@
 
   var ChatUI = ChatApp.ChatUI = function(chat) {
     this.chat = chat;
+    this.$messages = $('messages');
+    this.$input = $('textarea');
     this.bindEvents();  
+  };
+
+  ChatUI.prototype.processInput = function() {
+    var input = this.$input.val();
+    if (input[0] === '/') {
+      this.chat.processCommand(input.slice(1));
+    } else {
+      this.chat.sendMessage(input);
+    }
+    this.$input.val('');
+  };
+
+  ChatUI.prototype.alertName = function(data) {
+    var $div = $('<div>');
+    var text;
+    if (data.success) {
+      if (!data.prev) {
+        text = (data.nickname + " joined the chat.")
+      } else {
+        text = (data.prev + " is now known as " + data.nickname)
+      }
+    } else {
+      text = (data.message)
+    }
+    this.$messages.append($div.text(text));
   };
 
   ChatUI.prototype.bindEvents = function() {
@@ -13,14 +40,18 @@
 
     $('form').on('submit', function(event){
       event.preventDefault();
-      that.chat.sendMessage($('textarea').val());
-      $('textarea').val('');
+      that.processInput()
     });
 
     this.chat.socket.on('message', function(message){
-      $safeMessage = $('<div>').text(message.text); 
-      $('messages').append($safeMessage);
-    })
+      var string = message.nickname + ": " + message.text;
+      $safeMessage = $('<div>').text(string); 
+      that.$messages.append($safeMessage);
+    });
+
+    this.chat.socket.on('nicknameChangeResult', function(data) {
+      that.alertName(data);
+    });
   };
   
 })();
